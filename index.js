9a5969a35541d3ed9b8e437f28f5dbe8b94c8a8e
Mac <mac@Macs-MacBook-Air.local>
2023-01-11 15:28:41 +0630

    done_all

--- index.js
+++ index.js
@@ -39,8 +39,49 @@ app.post('/add-new',async (req, res) => {
     res.send(result);
 })
 
+app.get('/products', async (req, res) => {
+    let result = await Product.find();
 
+    if (result.length > 0) {
+        res.send(result)
+    } else {
+        res.send(false)
+    }
+})
 
+app.delete('/products/:id', async (req, res) => {
+    let result = await Product.deleteOne({ _id: req.params.id });
+    res.send(result);
+})
+
+app.get('/products/:id', async (req, res) => {
+    let result = await Product.findOne({ _id: req.params.id });
+
+    if (result) {
+        res.send(result)
+    } else {
+        res.send(false)
+    }
+})
+
+app.put('/products/:id', async (req, res) => {
+    let result = await Product.updateOne(
+        { _id: req.params.id }, {
+            $set:req.body
+        }
+    )
+    res.send(result);
+})
+
+app.get('/search/:key', async (req, res) => {
+    let result = await Product.find({
+        $or:[{category:{$regex:req.params.key}}]
+    })
+    
+    
+        res.send(result);
+   
+})
 
 app.listen(7000, () => {
     console.log("working")
